5de9eee6ab000c5b42a3c336602b463ee4d185ea
Angie <amaticorena103@hotmail.com>
2021-11-15 20:43:27 -0500

    commit tree cc4bd32a04290aa4ab8a1981fe1bef3c2b19ba0d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, nameOfDriver) {
+    return drivers.filter(function(driver){
+        return driver.toLowerCase() === nameOfDriver.toLowerCase()    
+    })
+}
+
+function fuzzyMatch(drivers, nameOfDriver) {
+   return drivers.filter(function(driver){
+       return driver.slice(0, 2) === nameOfDriver.slice(0,2)
+    })
+}
+
+function matchName(drivers, nameOfDriver) {
+   return drivers.filter(function(driver) {
+        return driver.name === nameOfDriver
+    } )
+}
